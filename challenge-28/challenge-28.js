8c78aca0c26e6e9703c4681afef6c5fdedd985d6
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-01-24 20:42:03 -0300

    buscando CEP, apenas no console

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,14 +1,108 @@
-  /*
-  No HTML:
-  - Crie um formulário com um input de texto que receberá um CEP e um botão
-  de submit;
-  - Crie uma estrutura HTML para receber informações de endereço:
-  "Logradouro, Bairro, Estado, Cidade e CEP." Essas informações serão
-  preenchidas com os dados da requisição feita no JS.
-  - Crie uma área que receberá mensagens com o status da requisição:
-  "Carregando, sucesso ou erro."
+function DOM(elements) {
+  this.element = this.getDOMElements(elements);
+}
 
-  No JS:
+DOM.prototype.getDOMElements = function getDOMElements(elements){
+  return document.querySelectorAll(elements);
+};
+
+DOM.prototype.on = function on(eventType, callback) {
+  Array.prototype.forEach.call(this.element, function(element) {
+    element.addEventListener(eventType, callback, false)
+  })
+};
+
+DOM.prototype.off = function off(eventType, callback) {
+  Array.prototype.forEach.call(this.element, function(element) {
+    element.removeEventListener(eventType, callback, false)
+  });
+};
+
+DOM.prototype.get = function get() {
+  return this.element;
+};
+
+DOM.prototype.forEach = function forEach() {
+  return Array.prototype.forEach.apply(this.element, arguments);
+};
+
+DOM.prototype.map = function map() {
+  return Array.prototype.map.apply(this.element, arguments);
+};
+DOM.prototype.filter = function filter() {
+  return Array.prototype.filter.apply(this.element, arguments);
+};
+
+DOM.prototype.reduce = function reduce() {
+  return Array.prototype.reduce.apply(this.element, arguments);
+};
+
+DOM.prototype.reduceRight = function reduceRight() {
+  return Array.prototype.reduceRight.apply(this.element, arguments);
+};
+
+DOM.prototype.every = function every() {
+  return Array.prototype.every.apply(this.element, arguments);
+};
+
+DOM.prototype.some = function some() {
+  return Array.prototype.some.apply(this.element, arguments);
+};
+
+DOM.prototype.isArray = function isArray(param) {
+  return Object.prototype.toString.call(param) === '[object Array]';
+};
+
+DOM.prototype.isObject = function isObject(param) {
+  return Object.prototype.toString.call(param) === '[object Object]';
+};
+
+DOM.prototype.isFunction = function isFunction(param) {
+  return Object.prototype.toString.call(param) === '[object Function]';
+};
+
+DOM.prototype.isNumber = function isNumber(param) {
+  return Object.prototype.toString.call(param) === '[object Number]';
+};
+
+DOM.prototype.isString = function isString(param) {
+  return Object.prototype.toString.call(param) === '[object String]';
+};
+
+DOM.prototype.isBoolean = function isBoolean(param) {
+  return Object.prototype.toString.call(param) === '[object Boolean]';
+};
+
+DOM.prototype.isNull = function isNull(param) {
+  return Object.prototype.toString.call(param) === '[object Null]'
+  || Object.prototype.toString.call(param) === '[object Undefined]';
+};
+
+var formCEP =  new DOM('[data-js="formCep"]');
+var inputCEP = new DOM('[ data-js="inicial"]')
+var ajax = new XMLHttpRequest();
+formCEP.on('submit', handleSubmitFormCEP);
+
+function handleSubmitFormCEP(event){
+  event.preventDefault();
+  var url = 'https://viacep.com.br/ws/[CEP]/json/'.replace('[CEP]', inputCEP.get()[0].value);
+  ajax.open('GET', url);
+  ajax.send();
+  ajax.addEventListener('readystatechange', handleReadyStateChange)
+}
+
+function  handleReadyStateChange() {
+  if(ajax.readyState === 4 && ajax.status === 200) {
+    console.log('Popular formulário', ajax.responseText);
+  }
+  console.log('carregando...');
+}
+
+
+
+
+  /*
+   No JS:
   - O CEP pode ser entrado pelo usuário com qualquer tipo de caractere, mas
   deve ser limpo e enviado somente os números para a requisição abaixo;
   - Ao submeter esse formulário, deve ser feito um request Ajax para a URL:
